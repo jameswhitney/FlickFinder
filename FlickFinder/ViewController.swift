96e99b1d75c77004824499fddbee548853cbc4dd
James Whitney <jamesdrums@mac.com>
2018-04-04 13:20:06 -0400

    feat: Finds random page from data
    Implements all checks as previous function but adds the ability
    to randomly select pages as opposed to selecting random images from
    the first page.

--- FlickFinder/ViewController.swift
+++ FlickFinder/ViewController.swift
@@ -110,175 +110,185 @@ class ViewController: UIViewController {
     
     // MARK: Flickr API
     
-    private func displayImageFromFlickrBySearch(_ methodParameters: [String: AnyObject]) {
-        // TODO: Make request to Flickr!
+//    private func displayImageFromFlickrBySearch(_ methodParameters: [String: AnyObject]) {
+//        // TODO: Make request to Flickr!
+//
+//        // create URLSession and URLRequest
+//        let session = URLSession.shared
+//        let request = URLRequest(url: flickrURLFromParameters(methodParameters))
+//
+//        // create network request
+//        let task = session.dataTask(with: request) { (data, response, error) in
+//
+//            func displayError(_ error: String) {
+//                print(error)
+//                performUIUpdatesOnMain {
+//                    self.setUIEnabled(true)
+//                    self.photoTitleLabel.text = "No photo found. Please try again."
+//                    self.photoImageView.image = nil
+//                }
+//            }
+//
+//            // Check if there was an error
+//            guard (error == nil) else {
+//                displayError("There was an error with your request: \(error ?? "Whoops, something went wrong" as! Error)")
+//                return
+//            }
+//
+//            // Check for successful response in the 2xx space
+//            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+//                displayError("Your request returned a status code other than 2xx!")
+//                return
+//            }
+//
+//            // Check if data was returned
+//            guard let data = data else {
+//                displayError("No data was returned by request!")
+//                return
+//            }
+//
+//            // parse the data
+//            let parsedResult: [String:AnyObject]!
+//            do {
+//                parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:AnyObject]
+//            } catch {
+//                displayError("Could not parse as JSON: \(data)")
+//                return
+//            }
+//
+//            // Did we recieve a return error (stat != ok)?
+//            guard let stat = parsedResult[Constants.FlickrResponseKeys.Status] as? String, stat == Constants.FlickrResponseValues.OKStatus else {
+//                displayError("Flickr API returned an error. See code and message in \(parsedResult)")
+//                return
+//            }
+//
+//
+//            guard let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String:AnyObject], let photoArray = photosDictionary[Constants.FlickrResponseKeys.Photo] as? [[String:AnyObject]] else {
+//                displayError("Cannot find keys '\(Constants.FlickrResponseKeys.Photos)' and '\(Constants.FlickrResponseKeys.Photo)' in \(parsedResult)")
+//                return
+//            }
+//
+//            // select a random photo
+//            let randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
+//            let photoDictionary = photoArray[randomPhotoIndex] as [String:AnyObject]
+//            let photoTitle = photoDictionary[Constants.FlickrResponseKeys.Title] as? String
+//
+//            // Does photo url contain our 'url_m'?
+//            guard let imageUrlString = photoDictionary[Constants.FlickrResponseKeys.MediumURL] as? String else {
+//                displayError("Key '\(Constants.FlickrResponseKeys.MediumURL)' in \(photoArray)")
+//                return
+//            }
+//
+//            // if an image exists at url, set the image an title
+//            let imageURL = URL(string: imageUrlString)
+//            if let imageData = try? Data(contentsOf: imageURL!) {
+//                performUIUpdatesOnMain {
+//                    self.setUIEnabled(true)
+//                    self.photoImageView.image = UIImage(data: imageData)
+//                    self.photoTitleLabel.text = photoTitle ?? "(Untitled)"
+//                }
+//            } else {
+//                displayError("Image does not exist at \(String(describing: imageURL))")
+//                }
+//            }
+//        task.resume()
+//        }
+//    }
+//
+//
+//
+//    // MARK: Helper for Creating a URL from Parameters
+//
+//    private func flickrURLFromParameters(_ parameters: [String: AnyObject]) -> URL {
+//
+//        var components = URLComponents()
+//        components.scheme = Constants.Flickr.APIScheme
+//        components.host = Constants.Flickr.APIHost
+//        components.path = Constants.Flickr.APIPath
+//        components.queryItems = [URLQueryItem]()
+//
+//        for (key, value) in parameters {
+//            let queryItem = URLQueryItem(name: key, value: "\(value)")
+//            components.queryItems!.append(queryItem)
+//        }
+//
+//        return components.url!
+//    }
+//
+//
+//// MARK: - ViewController: UITextFieldDelegate
+//
+//extension ViewController: UITextFieldDelegate {
+//
+//    // MARK: UITextFieldDelegate
+//
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        textField.resignFirstResponder()
+//        return true
+//    }
+//
+//    // MARK: Show/Hide Keyboard
+//
+//    func keyboardWillShow(_ notification: Notification) {
+//        if !keyboardOnScreen {
+//            view.frame.origin.y -= keyboardHeight(notification)
+//        }
+//    }
+//
+//    func keyboardWillHide(_ notification: Notification) {
+//        if keyboardOnScreen {
+//            view.frame.origin.y += keyboardHeight(notification)
+//        }
+//    }
+//
+//    func keyboardDidShow(_ notification: Notification) {
+//        keyboardOnScreen = true
+//    }
+//
+//    func keyboardDidHide(_ notification: Notification) {
+//        keyboardOnScreen = false
+//    }
+//
+//    func keyboardHeight(_ notification: Notification) -> CGFloat {
+//        let userInfo = (notification as NSNotification).userInfo
+//        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+//        return keyboardSize.cgRectValue.height
+//    }
+//
+//    func resignIfFirstResponder(_ textField: UITextField) {
+//        if textField.isFirstResponder {
+//            textField.resignFirstResponder()
+//        }
+//    }
+//
+//    @IBAction func userDidTapView(_ sender: AnyObject) {
+//        resignIfFirstResponder(phraseTextField)
+//        resignIfFirstResponder(latitudeTextField)
+//        resignIfFirstResponder(longitudeTextField)
+//    }
+//
+//    // MARK: TextField Validation
+//
+//    func isTextFieldValid(_ textField: UITextField, forRange: (Double, Double)) -> Bool {
+//        if let value = Double(textField.text!), !textField.text!.isEmpty {
+//            return isValueInRange(value, min: forRange.0, max: forRange.1)
+//        } else {
+//            return false
+//        }
+//    }
+//
+//    func isValueInRange(_ value: Double, min: Double, max: Double) -> Bool {
+//        return !(value < min || value > max)
+//    }
+    
+    private func displayImageFromFlickrBySearch(_ methodParameters: [String:AnyObject], withPageNumber: Int) {
         
-        // create URLSession and URLRequest
-        let session = URLSession.shared
-        let request = URLRequest(url: flickrURLFromParameters(methodParameters))
+        // ad the page to the method's parameters
+        var methodParametersWithPageNumber = methodParameters
+        methodParametersWithPageNumber[Constants.FlickrParameterKeys.Page] = withPageNumber as AnyObject?
         
-        // create network request
-        let task = session.dataTask(with: request) { (data, response, error) in
-            
-            func displayError(_ error: String) {
-                print(error)
-                performUIUpdatesOnMain {
-                    self.setUIEnabled(true)
-                    self.photoTitleLabel.text = "No photo found. Please try again."
-                    self.photoImageView.image = nil
-                }
-            }
-            
-            // Check if there was an error
-            guard (error == nil) else {
-                displayError("There was an error with your request: \(error ?? "Whoops, something went wrong" as! Error)")
-                return
-            }
-            
-            // Check for successful response in the 2xx space
-            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
-                displayError("Your request returned a status code other than 2xx!")
-                return
-            }
-            
-            // Check if data was returned
-            guard let data = data else {
-                displayError("No data was returned by request!")
-                return
-            }
-            
-            // parse the data
-            let parsedResult: [String:AnyObject]!
-            do {
-                parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:AnyObject]
-            } catch {
-                displayError("Could not parse as JSON: \(data)")
-                return
-            }
-            
-            // Did we recieve a return error (stat != ok)?
-            guard let stat = parsedResult[Constants.FlickrResponseKeys.Status] as? String, stat == Constants.FlickrResponseValues.OKStatus else {
-                displayError("Flickr API returned an error. See code and message in \(parsedResult)")
-                return
-            }
-            
-            
-            guard let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String:AnyObject], let photoArray = photosDictionary[Constants.FlickrResponseKeys.Photo] as? [[String:AnyObject]] else {
-                displayError("Cannot find keys '\(Constants.FlickrResponseKeys.Photos)' and '\(Constants.FlickrResponseKeys.Photo)' in \(parsedResult)")
-                return
-            }
-            
-            // select a random photo
-            let randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
-            let photoDictionary = photoArray[randomPhotoIndex] as [String:AnyObject]
-            let photoTitle = photoDictionary[Constants.FlickrResponseKeys.Title] as? String
-            
-            // Does photo url contain our 'url_m'?
-            guard let imageUrlString = photoDictionary[Constants.FlickrResponseKeys.MediumURL] as? String else {
-                displayError("Key '\(Constants.FlickrResponseKeys.MediumURL)' in \(photoArray)")
-                return
-            }
-            
-            // if an image exists at url, set the image an title
-            let imageURL = URL(string: imageUrlString)
-            if let imageData = try? Data(contentsOf: imageURL!) {
-                performUIUpdatesOnMain {
-                    self.setUIEnabled(true)
-                    self.photoImageView.image = UIImage(data: imageData)
-                    self.photoTitleLabel.text = photoTitle ?? "(Untitled)"
-                }
-            } else {
-                displayError("Image does not exist at \(String(describing: imageURL))")
-                }
-            }
-        task.resume()
-        }
-    }
-
-    
-    
-    // MARK: Helper for Creating a URL from Parameters
-    
-    private func flickrURLFromParameters(_ parameters: [String: AnyObject]) -> URL {
         
-        var components = URLComponents()
-        components.scheme = Constants.Flickr.APIScheme
-        components.host = Constants.Flickr.APIHost
-        components.path = Constants.Flickr.APIPath
-        components.queryItems = [URLQueryItem]()
         
-        for (key, value) in parameters {
-            let queryItem = URLQueryItem(name: key, value: "\(value)")
-            components.queryItems!.append(queryItem)
-        }
-        
-        return components.url!
-    }
-
-
-// MARK: - ViewController: UITextFieldDelegate
-
-extension ViewController: UITextFieldDelegate {
-    
-    // MARK: UITextFieldDelegate
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
-    // MARK: Show/Hide Keyboard
-    
-    func keyboardWillShow(_ notification: Notification) {
-        if !keyboardOnScreen {
-            view.frame.origin.y -= keyboardHeight(notification)
-        }
-    }
-    
-    func keyboardWillHide(_ notification: Notification) {
-        if keyboardOnScreen {
-            view.frame.origin.y += keyboardHeight(notification)
-        }
-    }
-    
-    func keyboardDidShow(_ notification: Notification) {
-        keyboardOnScreen = true
-    }
-    
-    func keyboardDidHide(_ notification: Notification) {
-        keyboardOnScreen = false
-    }
-    
-    func keyboardHeight(_ notification: Notification) -> CGFloat {
-        let userInfo = (notification as NSNotification).userInfo
-        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
-        return keyboardSize.cgRectValue.height
-    }
-    
-    func resignIfFirstResponder(_ textField: UITextField) {
-        if textField.isFirstResponder {
-            textField.resignFirstResponder()
-        }
-    }
-    
-    @IBAction func userDidTapView(_ sender: AnyObject) {
-        resignIfFirstResponder(phraseTextField)
-        resignIfFirstResponder(latitudeTextField)
-        resignIfFirstResponder(longitudeTextField)
-    }
-    
-    // MARK: TextField Validation
-    
-    func isTextFieldValid(_ textField: UITextField, forRange: (Double, Double)) -> Bool {
-        if let value = Double(textField.text!), !textField.text!.isEmpty {
-            return isValueInRange(value, min: forRange.0, max: forRange.1)
-        } else {
-            return false
-        }
-    }
-    
-    func isValueInRange(_ value: Double, min: Double, max: Double) -> Bool {
-        return !(value < min || value > max)
     }
 }
 
